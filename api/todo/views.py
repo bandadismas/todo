01a9d4d0b113fb1069f94d96665034442f9c25a4
Dismas Banda <bandadismas@gmail.com>
2021-06-03 15:21:50 +0200

    feat: Create a task
    Signed-off-by: Dismas Banda <bandadismas@gmail.com>

--- api/todo/views.py
+++ api/todo/views.py
@@ -11,3 +11,12 @@ def taskList(request):
 	tasks = Task.objects.all().order_by('-id')
 	serializer = TaskSerializer(tasks, many=True)
 	return Response(serializer.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+	serializer = TaskSerializer(data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
