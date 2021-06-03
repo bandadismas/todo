526e208338c43b691fa8ba148d21e6d707bf6ffb
Dismas Banda <bandadismas@gmail.com>
2021-06-03 17:45:14 +0200

    White list port 3000 to access the server
    Signed-off-by: Dismas Banda <bandadismas@gmail.com>

--- api/api/settings.py
+++ api/api/settings.py
@@ -128,3 +128,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+
+    "http://localhost:3000",
+
+]
