0667747bf9564bb2cb094fc51ffdb538f20996ea
Dismas Banda <bandadismas@gmail.com>
2021-06-03 15:33:48 +0200

    feat: Create a url for task details
    Signed-off-by: Dismas Banda <bandadismas@gmail.com>

--- api/todo/urls.py
+++ api/todo/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
 	path('task-list/', views.taskList, name="task-list"),
 	path('task-create/', views.taskCreate, name="task-create"),
+	path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
+
 ]
